45a7f87f9a47c414c156183082a8d8b1ae64c670
nikanapirelieristavi <143445022+nikanapirelieristavi@users.noreply.github.com>
2024-03-23 05:41:47 -0700

    Add files via upload

---
+++ day 48/homework/script.js
@@ -0,0 +1,34 @@
+// 2
+const pi = 3.14;
+const radius = 10;
+const area = pi * (r*2);
+const length = 2 * pi * r;
+// 3
+let num1 = 21;
+let num2 = 21;
+console.log( num1 < num2); 
+console.log( num1 > num2); 
+console.log( num1 <= num2); 
+console.log( num1 >= num2); 
+console.log( num1 === num2); 
+console.log( num1 !== num2);
+// 4
+const book = "the catcher in the rye";
+const pages = 212;
+console.log("In ", book, " there are ", pages, " pages")
+// 5
+//name ver gamoviyene radgan rogorc chans msgavsi saxelis operatori arsebobs javascriptshi
+const naim = "nika";
+const surname = "napireli-eristavi";
+const age = "15";
+const address = "Ninoshvili"
+console.log("name: ", naim, "surname: ", surname, "age: ", age, "address: ", address)
+// 6
+number1 = 30;
+number2 = -30;
+console.log(number1 - number2)
+console.log(number1 + number2)
+console.log(number1 % number2)
+console.log(number1 * number2)
+console.log(number2 - number1)
+console.log(number2 + number1)
